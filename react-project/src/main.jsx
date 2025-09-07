0120dfd8221bf7bc990adb3c92ccbc8f74233333
Ashley Christensen <reactandfloat@gmail.com>
2025-09-07 15:40:07 -0700

    destructured

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const [firstAnimal, secondAnimal, thirdAnimal] = ["giraffe", "zebra", "lion", "tiger", "bear"];
+console.log(secondAnimal) ;
+
 createRoot(document.getElementById('root')).render(
     <App />
 )
